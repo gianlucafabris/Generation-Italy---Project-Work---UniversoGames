39030c6ef6e67039498e4d50ab5b59c706798b02
Dave <69114944+creambastard@users.noreply.github.com>
2022-06-09 18:07:04 +0200

    giornata lavorativa 1
    aggiunta IDaoImmagini
    aggiunta DaoImmagini

--- ProjectWork/cmsnewsrecensioni/src/main/java/com/universogames/cmsnewsrecensioni/entities/Immagine.java
+++ ProjectWork/cmsnewsrecensioni/src/main/java/com/universogames/cmsnewsrecensioni/entities/Immagine.java
@@ -3,9 +3,13 @@ package com.universogames.cmsnewsrecensioni.entities;
 public class Immagine {
 	
 	private String nome;
+	private int idNotizia;
+	private int idRecensione;
 
-	public Immagine(String nome) {
+	public Immagine(String nome, int idNotizia, int idRecensione) {
 		this.nome = nome;
+		this.idNotizia = idNotizia;
+		this.idRecensione = idRecensione;
 	}
 
 	public String getNome() {
@@ -15,5 +19,23 @@ public class Immagine {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public int getIdNotizia() {
+		return idNotizia;
+	}
+
+	public void setIdNotizia(int idNotizia) {
+		this.idNotizia = idNotizia;
+	}
+
+	public int getIdRecensione() {
+		return idRecensione;
+	}
+
+	public void setIdRecensione(int idRecensione) {
+		this.idRecensione = idRecensione;
+	}
+
+	
 	
 }
